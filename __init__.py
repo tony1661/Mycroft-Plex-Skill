e053053d8396359b6f371172413e1edbd72fe020
Tony Fernandez <tfernandez.sc2@gmail.com>
2019-04-24 21:12:32 -0400

    started to add the ability to get radarr to download a movie

--- __init__.py
+++ __init__.py
@@ -4,15 +4,32 @@ from mycroft.util.log import LOG
 import requests # for curl
 import xml.etree.ElementTree as ET #xml parser
 
-# Variables
-XPlexToken = 'aW9gR8bPYbbpsQzZ3m7q'
+# General Variables
 host = '192.168.88.22'
+# Plex Variables
+XPlexToken = 'aW9gR8bPYbbpsQzZ3m7q'
+# Radarr Variables
+radarr_port = '7878'
+radarr_api_key = '528d3d2326734853a39d6a38feec02d0'
 
 class TemplateSkill(MycroftSkill):
 
     def __init__(self):
         super(TemplateSkill, self).__init__(name="TemplateSkill")
+        self.test = ''
 
+    @intent_handler(IntentBuilder("").require("AddMovie"))
+    def handle_addamovie_intent(self, message):
+        self.speak_dialog("which.movie")
+        #self.speak_dialog("which.movie")
+        #r = self.get_response('which.movie', num_retries=1)
+        #print(r)
+        # params = (
+        #     ('X-Plex-Token', XPlexToken),
+        # )
+        # response = requests.get('http://' + host + ':32400/library/sections/1/refresh', params=params)
+        
+        
 
     @intent_handler(IntentBuilder("").require("RefreshPlex"))
     def handle_refreshplex_intent(self, message):
@@ -24,7 +41,7 @@ class TemplateSkill(MycroftSkill):
         self.speak_dialog("refreshing.plex")
 
     @intent_handler(IntentBuilder("").require("CountMovies"))
-    def handle_refreshplex_intent(self, message):
+    def handle_countmovies_intent(self, message):
         count = 0
         params = (
             ('X-Plex-Token', XPlexToken),
